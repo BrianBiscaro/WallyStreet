fabaa5efeef887058c6cfa508d768e84d72e9ade
BrianBiscaro <brianbiscaro22@gmail.com>
2026-07-16 22:40:57 -0300

    env api url

--- wallystreet-front/src/api/api.js
+++ wallystreet-front/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://172.30.58.55:80",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
